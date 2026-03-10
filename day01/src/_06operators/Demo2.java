c176294cf4e6f9324f503b9342befb7fb9ea52e5
heidou <294925572@qq.com>
2026-03-11 03:02:16 +0800

    赋值运算符

---
+++ day01/src/_06operators/Demo2.java
@@ -0,0 +1,19 @@
+package _06operators;
+
+/*
+    赋值运算符：
+    +=：加等于
+    -=：减等于
+    *=：乘等于
+    /=：除等于
+*/
+public class Demo2 {
+    public static void main(String[] args) {
+        int a = 11, b = 2;
+        // 复合赋值运算符
+        a += b; // a = a + b;
+        a -= b; // a = a - b;
+        a *= b; // a = a * b;
+        a /= b; // a = a / b;
+    }
+}
