7823ac1130c73d0b74b1531f764227b0310af679
heidou <294925572@qq.com>
2026-03-11 03:02:56 +0800

    关系运算符

---
+++ day01/src/_06operators/Demo3.java
@@ -0,0 +1,22 @@
+package _06operators;
+
+/*
+    关系运算符：
+    >：大于
+    <：小于
+    >=：大于等于
+    <=：小于等于
+    ==：等于
+    !=：不等于
+*/
+public class Demo3 {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        System.out.println(a > b); // false
+        System.out.println(a < b); // true
+        System.out.println(a >= b);// false
+        System.out.println(a <= b); // true
+        System.out.println(a == b); // false
+        System.out.println(a != b);// true
+    }
+}
