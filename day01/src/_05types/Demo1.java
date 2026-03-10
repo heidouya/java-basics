7a5addda1abe0dc7220d3a0afff6552597060e0b
heidou <294925572@qq.com>
2026-03-11 03:01:21 +0800

    基本数据类型

---
+++ day01/src/_05types/Demo1.java
@@ -0,0 +1,25 @@
+package _05types;
+
+// 基本数据类型
+public class Demo1 {
+    public static void main(String[] args) {
+        // 整型
+        byte a = 1;
+        short b = 1000;
+        int c = 10000;
+        long d = 100000L;
+
+        // 浮点类型
+        float e = 0.1f; // 单精度
+        double f = 0.1; // 双精度
+
+        // 字符
+        char g = 'e';
+        // 字符串它是引用数据类型，后面聊
+        String h = "fff";
+
+        // 布尔
+        boolean flag1 = true;
+        boolean flag2 = false;
+    }
+}
