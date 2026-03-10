112077b29828eb7992927cb9013a341877600877
heidou <294925572@qq.com>
2026-03-11 03:04:51 +0800

    do…while 循环

---
+++ day01/src/_08loops/Demo2.java
@@ -0,0 +1,19 @@
+package _08loops;
+
+/*
+    do…while 循环格式：
+    do {
+      //代码语句
+    } while(条件表达式);
+
+    do…while 循环和 while 循环相似，不同的是，do…while 循环至少会执行一次。
+*/
+public class Demo2 {
+    public static void main(String[] args) {
+        int a = 2;
+        do {
+            System.out.println(a); // 2 1
+            a--;
+        } while(a > 0);
+    }
+}
