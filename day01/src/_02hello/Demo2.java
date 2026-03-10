bcbd028aaa10146d372d83429f2afb4befccd7af
heidou <294925572@qq.com>
2026-03-11 02:57:31 +0800

    IDEA快捷键

---
+++ day01/src/_02hello/Demo2.java
@@ -0,0 +1,19 @@
+package _02hello;
+
+/*
+    IDEA 常用快捷键：
+    1. 复制当前行：Ctrl + D
+    2. 删除当前行：Ctrl + Y
+    3. 上下移动代码行：Ctrl + Shift + Up/Down
+    4. 批量光标：Alt+Shift+鼠标左键单击
+    5. 格式化代码：Ctrl + Alt + L
+    6. 代码块包围：Ctrl + Alt + T
+    7. 注释代码：Ctrl + /：单行注释/取消注释。
+    8. 多行注释：Ctrl + Shift + /：块注释/取消注释。
+    9. 多功能快捷键：Alt + Enter
+    10. 全局搜索：Shift 连按两次
+*/
+public class Demo2 {
+    public static void main(String[] args) {
+    }
+}
