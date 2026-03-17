d383f2ad17057138d5cb1f2bc272db10db09dd91
heidou <294925572@qq.com>
2026-03-18 02:57:12 +0800

    简要介绍多态

---
+++ day02/src/_14polymorphism/HarmonyPhone.java
@@ -0,0 +1,13 @@
+package _14polymorphism;
+
+// 2. 子类1：鸿蒙手机（重写开机方法）
+public class HarmonyPhone extends HarmonyDevice {
+    @Override
+    public void powerOn() {
+        System.out.println("手机开机，自动连接蓝牙");
+    }
+
+    public void takePhoto() {
+        System.out.println("调用了手机拍照功能");
+    }
+}
