5e98b6260342a7788a0d4019140eca83edde421c
heidou <294925572@qq.com>
2026-03-28 10:00:46 +0800

    将静态方法、静态变量相关内容放到一个新的包中

---
+++ day02/src/_15static/Demo2.java
@@ -0,0 +1,33 @@
+package _15static;
+
+/*静态变量示例1 - 单例模式*/
+public class Demo2 {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+        System.out.println(obj1 == obj2);  // true
+    }
+}
+
+class Singleton {
+    // 静态变量保存唯一实例
+    private static Singleton instance;
+
+    // 私有构造
+    private Singleton() {
+        System.out.println("单例创建");
+    }
+
+    // 静态方法获取实例
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    // 静态方法业务逻辑
+    public static void doSomething() {
+        System.out.println("执行操作");
+    }
+}
