6e0568846b77e71524458efd04ff83e10f94f426
heidou <294925572@qq.com>
2026-05-25 19:28:31 +0800

    算数运算符

--- day01/src/_06operators/Demo1.java
+++ day01/src/_06operators/Demo1.java
@@ -24,19 +24,22 @@ public class Demo1 {
         // 前自增 先给这个变量加1，然后再使用这个变量
         // 后自增 先使用这个变量，再给这个变量加一
         int g = 1;
-        System.out.println(g++); // 1 相当于 g = g + 1
+        System.out.println(g++); // 1
+        System.out.println(g); // 2
 
         int h = 1;
         System.out.println(++h); // 2
-        // h = h + 1
+        System.out.println(h); // 2
 
         // 自减 --
         // 前自减 先给这个变量减1，然后再使用这个变量
         // 后自减 先使用这个变量，再给这个变量减一
         int i = 2;
         System.out.println(i--); // 2 相当于 i = i - 1
+        System.out.println(i); // 1
 
         int j = 2;
         System.out.println(--j); // 1
+        System.out.println(j); // 1
     }
 }
