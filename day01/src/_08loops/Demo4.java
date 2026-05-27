349efbc1f85b36ffecf6c8ad6932e3367a881e0d
heidou <294925572@qq.com>
2026-05-27 19:40:27 +0800

    break、continue语句

--- day01/src/_08loops/Demo4.java
+++ day01/src/_08loops/Demo4.java
@@ -6,6 +6,32 @@ package _08loops;
 */
 public class Demo4 {
     public static void main(String[] args) {
+        System.out.println("------------break语句-----------");
+        // break
+        for (int i = 0; i < 5; i++) {
+            if (i == 2) break;
+            System.out.println(STR."i = \{i}");
+        }
+        System.out.println("------------continue语句-----------");
+        // continue
+        for (int i = 0; i < 5; i++) {
+            if (i == 2) continue;
+            System.out.println(STR."i = \{i}");
+        }
+        System.out.println("-----使用标签（Label）跳出外层循环 break语句-----");
+
+        outer:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j == 2) {
+                    break outer;
+                }
+                System.out.println(STR."j = \{j}");
+            }
+        }
+
+        System.out.println("------------while循环 break语句-----------");
+
         // break 跳出循环
         int i = 1;
         //while (i <= 5) {
@@ -16,6 +42,8 @@ public class Demo4 {
         //    i++;
         //}
 
+        System.out.println("------------while循环 continue语句-----------");
+
         // continue 跳过本次循环
         int j = 1;
         while (j <= 5) {
@@ -23,7 +51,7 @@ public class Demo4 {
                 j++;
                 continue;
             }
-            System.out.println("j = " + j);
+            System.out.println(STR."j = \{j}");
             j++;
         }
     }
