82f9939a4916667284d624be5e4071b18b41c08d
heidou <294925572@qq.com>
2026-06-01 20:53:27 +0800

    字符串

--- day01/src/_05types/Demo3.java
+++ day01/src/_05types/Demo3.java
@@ -13,6 +13,23 @@ package _05types;
 3. 放在字符串常量池里，相同内容复用，节省内存
 4. 用 equals() 比较内容，别用 ==
 
+字符串常量池是 Java 堆内存中的一块特殊区域，用于缓存字符串字面量（用双引号 "..." 定义的字符串），让相同内容的字符串复用同一个对象，从而节省内存、提升性能。
+
+Java 会做 2 件事：
+1. 去常量池里找有没有这个字符串
+2. 有 → 直接用；没有 → 创建一个放进去，下次用时再找
+
+1. 双引号创建的字符串 → 进字符串常量池，自动复用
+2. new String 创建的 → 不进池（或单独放），不共享
+3. == 比较地址，所以常量池里相同字符串会是 true
+4. 比较字符串内容永远用 equals ()
+
+intern()：将字符串放入字符串常量池，返回池中相同内容的字符串对象，如果池中没有则创建一个。示例：
+```java
+String s1 = new String("Hello");
+String s2 = s1.intern();
+```
+
 常用方法：
 1. length()：获取字符串长度
 2. equals()：比较字符串内容
@@ -28,35 +45,41 @@ package _05types;
 12. endsWith()：判断是否以某字符串结尾
 13. indexOf()：获取某字符或子字符串第一次出现的位置
 14. lastIndexOf()：获取某字符或子字符串最后一次出现的位置
-
-字符串常量池（String Constant Pool）
-Java 专门给字符串开辟的一块内存区域，用来存字符串，避免重复创建，节省内存。
-
-Java 会做 2 件事：
-1. 去常量池里找有没有这个字符串
-2. 有 → 直接用；没有 → 创建一个放进去，下次用时再找
-
-1. 双引号创建的字符串 → 进字符串常量池，自动复用
-2. new String 创建的 → 不进池（或单独放），不共享
-3. == 比较地址，所以常量池里相同字符串会是 true
-4. 比较字符串内容永远用 equals ()
-
 */
 public class Demo3 {
     public static void main(String[] args) {
+        // 字符串存储
+        System.out.println((int)'a'); // 97
+        System.out.println((int)'我'); // 25105
+        System.out.println((char)25105);
+
         // 直接创建（推荐）
         String s1 = "Hello";
         String s2 = "Hello";
-        System.out.println(s1 == s2);           // 比较地址（相同内容复用）
+        // 比较地址（相同内容复用）
+        System.out.println(s1 == s2);   // true
         // 构造创建
         String s3 = new String("Hello");
+        System.out.println(s1 == s3);   // false
 
-        System.out.println(s1.length());          // 长度
-        System.out.println(s1.equals("abc"));     // 比较内容（必须用）
-        System.out.println(s1.contains("b"));     // 是否包含
-        System.out.println(s1.substring(2));      // 截取
-        System.out.println(s1.replace("a","b"));  // 替换
-        System.out.println(s1.split(","));        // 分割
-        System.out.println(s1.charAt(0));         // 取第1个字符
+        // intern()：将字符串放入字符串常量池，返回池中相同内容的字符串对象，如果池中没有则创建一个。
+        String s4 = s3.intern();
+        System.out.println(s1 == s4);   // true
+        System.out.println(s1 == s3); // false
+
+        // 获取字符串长度
+        System.out.println(s1.length());          // 5
+        // 比较字符串内容
+        System.out.println(s1.equals("abc"));     // false
+        // 判断是否包含某子字符串
+        System.out.println(s1.contains("b"));     // false
+        // 截取字符串
+        System.out.println(s1.substring(2));    // "llo"
+        // 替换字符串
+        System.out.println(s1.replace("a","b"));  // "bello"
+        // 分割字符串
+        System.out.println(s1.split(","));  // ["Hello"]
+        // 获取字符
+        System.out.println(s1.charAt(0));   // 'H'
     }
 }
