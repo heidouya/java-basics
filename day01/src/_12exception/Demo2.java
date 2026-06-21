fa91e27da84d5f7744c802008d83149e44c0adbc
heidou <294925572@qq.com>
2026-06-22 02:12:54 +0800

    try-with-resources 语句

--- day01/src/_12exception/Demo2.java
+++ day01/src/_12exception/Demo2.java
@@ -18,7 +18,8 @@ public class Demo2 {
             System.out.println("不管走 try 还是 catch 都会走finally里的代码，可以做一些清理工作");
         }
 
-        //try-with-resources 语句是一种用于自动管理资源的机制。它在 Java 7 中引入，旨在简化资源管理，确保资源在使用完毕后自动关闭，从而避免资源泄漏问题。
+        //try-with-resources 语句是一种用于自动管理资源的机制。它在 Java 7 中引入，实现了 `AutoCloseable` 的流可以自动关闭，
+        // 旨在简化资源管理，确保资源在使用完毕后自动关闭，从而避免资源泄漏问题。
         try (
                 FileReader fr = new FileReader("test.txt");
                 BufferedReader br = new BufferedReader(fr)
