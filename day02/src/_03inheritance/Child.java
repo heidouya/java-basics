3af91e1f019e339775b6fd4c75d4063e633d5645
heidou <294925572@qq.com>
2026-09-10 03:39:12 +0800

    final

--- day02/src/_03inheritance/Child.java
+++ day02/src/_03inheritance/Child.java
@@ -1,10 +1,10 @@
 package _03inheritance;
 
 // final修饰的类，禁止继承
-/*public class Child extends Parent{
-}*/
-
 /*
+public class Child extends Parent{
+
+}
 public class Child extends Parent {
     //final修饰方法 - 禁止重写
     @Override
@@ -15,14 +15,22 @@ public class Child extends Parent {
 */
 
 public class Child {
+    // final 修饰的成员变量在声明时赋值后，后续不能修改，在构造函数中也不行
     final int age;
 
+    // final变量必须在声明时赋值或在构造函数中赋值
     public Child(int age) {
         // final变量可以在构造函数中赋值
         this.age = age;
     }
 
+    public static void main(String[] args) {
+        Child child = new Child(10);
+        child.setAge(10);
+    }
     public void setAge(int age) {
-        //this.age = age;
+        // final变量不能被修改
+        final int age2 = 1;
+        //age2 = 3;
     }
 }
